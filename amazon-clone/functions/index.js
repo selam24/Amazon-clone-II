03abae2e121e45d010916e7ecfd3471601f25770
selam24 <109312381+selam24@users.noreply.github.com>
2024-09-27 03:18:45 -0400

    last commit

--- amazon-clone/functions/index.js
+++ amazon-clone/functions/index.js
@@ -1,64 +1,19 @@
-// Importing necessary Firebase and Node.js modules
-const { onRequest } = require("firebase-functions/v2/https"); // Importing Firebase Cloud Functions for HTTPS requests
-const logger = require("firebase-functions/logger"); // Logger for logging messages
-const express = require("express"); // Express framework for building the server
-const cors = require("cors"); // CORS middleware for handling cross-origin requests
-const dotenv = require("dotenv"); // Dotenv for managing environment variables
-const { setGlobalOptions } = require("firebase-functions/v2");
+/**
+ * Import function triggers from their respective submodules:
+ *
+ * const {onCall} = require("firebase-functions/v2/https");
+ * const {onDocumentWritten} = require("firebase-functions/v2/firestore");
+ *
+ * See a full list of supported triggers at https://firebase.google.com/docs/functions
+ */
 
-// Loading environment variables from the .env file
-dotenv.config();
+const {onRequest} = require("firebase-functions/v2/https");
+const logger = require("firebase-functions/logger");
 
-// Initializing Stripe with the secret key from environment variables
-const stripe = require("stripe")(process.env.STRIPE_KEY);
+// Create and deploy your first functions
+// https://firebase.google.com/docs/functions/get-started
 
-// Creating an instance of an Express application
-const app = express();
-
-setGlobalOptions({maxInstances: 10})
-
-// Using CORS to allow requests from any origin
-app.use(cors({ origin: true }));
-
-// Middleware to parse JSON bodies of incoming requests
-app.use(express.json());
-
-// Root endpoint to check if the server is running
-app.get("/", (req, res) => {
-  res.status(200).json({
-    message: "Success !", // Sending a success message
-  });
-});
-
-// Endpoint for creating a payment intent from the React app
-app.post("/Payment/create", async (req, res) => {
-  const total = parseInt(req.query.total); // Retrieving and parsing the total amount from the query parameters
-
-  // Check if the total amount is greater than 0
-  if (total > 0) {
-    try {
-      // Creating a payment intent with Stripe
-      const paymentIntent = await stripe.paymentIntents.create({
-        amount: total, // Amount in cents
-        currency: "usd", // Currency for the payment
-      });
-
-      // Sending the client secret back to the client
-      res.status(201).json({ clientSecret: paymentIntent.client_secret });
-    } catch (error) {
-      // Handling any errors that occur during payment intent creation
-      logger.error("Payment Intent creation failed:", error); // Logging the error
-      res
-        .status(500)
-        .json({ message: "Internal Server Error", error: error.message });
-    }
-  } else {
-    // Responding with an error if the total is not valid
-    res.status(403).json({
-      message: "total must be greater than 0", // Informing the client about the invalid total
-    });
-  }
-});
-
-// Exporting the Express app as a Firebase Cloud Function
-exports.api = onRequest(app);
+// exports.helloWorld = onRequest((request, response) => {
+//   logger.info("Hello logs!", {structuredData: true});
+//   response.send("Hello from Firebase!");
+// });
--- amazon-clone/src/API/axios.js
+++ amazon-clone/src/API/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://127.0.0.1:5001/clone-ii-fa077/us-central1/api",
+  baseURL: "https://amazon-api-2.onrender.com",
 });
 
 export { axiosInstance };
